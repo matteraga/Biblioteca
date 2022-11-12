28c972679d15c71a04cd24bbe2c320f9c5bf65df
Matteo Raganini <MATTEO.RAGANINI@local.ittcesena.it>
2022-11-12 09:07:38 +0100

    Aggiunti metodi per creare bibilioteca e libro

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,5 +24,39 @@ namespace Biblioteca
         {
             InitializeComponent();
         }
+
+        Biblioteca biblioteca;
+        private void btnBiblioteca_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtBiblioteca.Text != string.Empty)
+            {
+                string[] dati = txtBiblioteca.Text.Split('|');
+                if (dati.Length == 4)
+                {
+                    biblioteca = new Biblioteca(dati[0], dati[1], dati[2], dati[3], new List<Libro>());
+                    txtBiblioteca.Text = string.Empty;
+                }
+            }
+        }
+
+        private void btnLibro_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtLibro.Text != string.Empty && biblioteca != null) 
+            {
+                string[] dati = txtLibro.Text.Split('|');
+                if (dati.Length == 5)
+                {
+                    int annoPubblicazione;
+                    int nPagine;
+
+                    if (int.TryParse(dati[2], out annoPubblicazione) && int.TryParse(dati[4], out nPagine))
+                    {
+                        Libro libro = new Libro(dati[0], dati[1], annoPubblicazione, dati[3], nPagine);
+                        biblioteca.addLibro(libro);
+                        txtLibro.Text = string.Empty;
+                    }
+                }
+            }
+        }
     }
 }
